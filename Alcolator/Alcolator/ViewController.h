6948d16ed628fe0dfb1b1349b9012fd3064500ae
Luke Paulo <lukepaulo4@gmail.com>
2016-06-05 14:39:54 -0700

    Checkpoint 19 Interfaces: Complete

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Luke Paulo on 6/5/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
