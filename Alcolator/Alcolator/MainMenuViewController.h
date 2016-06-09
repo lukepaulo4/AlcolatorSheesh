75b5eb3b4774d73082ce1e92c39b3116e6fbf44c
Luke Paulo <lukepaulo4@gmail.com>
2016-06-08 18:07:42 -0700

    Checkpoint 21 Interfaces: Complete

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Luke Paulo on 6/8/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
